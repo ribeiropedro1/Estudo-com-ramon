2e7ada116875fe8b083323e8ff7ccaec0904fd43
ribeiropedro1 <141585011+ribeiropedro1@users.noreply.github.com>
2024-04-16 14:18:38 -0300

    Add files via upload

---
+++ Exercicios2/Testes/SobreEscrita.java
@@ -0,0 +1,10 @@
+package Exercicios2.Testes;
+
+import Dominio.Lampada;
+
+public class SobreEscrita {
+    public static void main(String[] args) {
+        Lampada lampada = new Lampada("Equinox", "Luminescente");
+        System.out.println(lampada);
+    }
+}
